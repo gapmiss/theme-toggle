4a640e2fb224551c2df6b086c7a93ab265c02ff2
@gapmiss <gapmiss@proton.me>
2025-03-16 15:48:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ribbon theme toggle
+
+An Obsidian.md plugin which adds a ribbon icon for toggling between dark & light themes.
+
---
+++ main.ts
@@ -0,0 +1,55 @@
+import { Plugin, setIcon } from 'obsidian';
+
+export default class ThemeToggle extends Plugin {
+
+  async onload() {
+
+    this.registerEvent(
+      this.app.workspace.on('css-change', () => {
+        setTimeout(() => {
+          let ribbonIcon: HTMLElement | null = window.document.querySelector('.ribbon-theme-toggle-plugin');
+          setIcon(ribbonIcon!, this.getCurrentTheme() === 'obsidian' ? 'sun' : 'moon');
+          ribbonIcon!.setAttr("aria-label","Use " + (this.getCurrentTheme() === 'obsidian' ? 'light' : 'dark') + " mode");
+        }, 10);
+      })
+    );
+
+    const ribbonIconEl = this.addRibbonIcon(this.getThemeIcon(), "Use " + (this.getCurrentTheme() === 'obsidian' ? 'light' : 'dark') + " mode", (evt: MouseEvent) => {
+      // @ts-ignore
+      this.app.changeTheme(this.getCurrentTheme() === 'obsidian' ? 'moonstone' : 'obsidian');
+      setIcon(evt.target as HTMLElement, this.getThemeIcon());
+      (evt.target as HTMLElement).setAttr("aria-label","Use " + (this.getCurrentTheme() === 'obsidian' ? 'light' : 'dark') + " mode");
+    });
+
+    ribbonIconEl.addClass('ribbon-theme-toggle-plugin');
+
+    this.addCommand({
+      id: "toggle-theme",
+      name: "Toggle theme",
+      callback: () => {
+        // @ts-ignore
+        this.app.changeTheme(this.getCurrentTheme() === 'obsidian' ? 'moonstone' : 'obsidian');
+        let ribbonIcon: HTMLElement | null = window.document.querySelector('.ribbon-theme-toggle-plugin');
+        setIcon(ribbonIcon!, this.getCurrentTheme() === 'obsidian' ? 'sun' : 'moon');
+      }
+    });
+
+    console.log('Theme toggle plugin loaded');
+  }
+
+  onunload() {
+    console.log('Theme toggle plugin unloaded');
+  }
+
+  getCurrentTheme() {
+    // @ts-ignore
+    let currentTheme: string = this.app.getTheme() === 'obsidian' ? 'obsidian' : 'moonstone';
+    return currentTheme;
+  }
+
+  getThemeIcon() {
+    let moonOrSunIcon = this.getCurrentTheme() === 'obsidian' ? 'sun' : 'moon';
+    return moonOrSunIcon;
+  }
+
+}
