4e2f76e72c0f7a9d31e757d5dbb2e5a0b5c3b1fc
gapmiss <98914514+gapmiss@users.noreply.github.com>
2025-05-05 19:37:02 -0500

    aria-label hint

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ export default class ThemeToggle extends Plugin {
         setTimeout(() => {
           let ribbonIcon: HTMLElement | null = window.document.querySelector('.ribbon-theme-toggle-plugin');
           setIcon(ribbonIcon!, this.getCurrentTheme() === 'obsidian' ? 'sun' : 'moon');
-          ribbonIcon!.setAttr("aria-label","Theme toggle: " + (this.getCurrentTheme() === 'obsidian' ? 'light' : 'dark') + " mode");
+          ribbonIcon!.setAttr("aria-label","Toggle to " + (this.getCurrentTheme() === 'obsidian' ? 'light' : 'dark') + " mode");
         }, 10);
       })
     );
@@ -18,7 +18,7 @@ export default class ThemeToggle extends Plugin {
       // @ts-ignore
       this.app.changeTheme(this.getCurrentTheme() === 'obsidian' ? 'moonstone' : 'obsidian');
       setIcon(evt.target as HTMLElement, this.getThemeIcon());
-      (evt.target as HTMLElement).setAttr("aria-label","Theme toggle: " + (this.getCurrentTheme() === 'obsidian' ? 'light' : 'dark') + " mode");
+      (evt.target as HTMLElement).setAttr("aria-label","Toggle to " + (this.getCurrentTheme() === 'obsidian' ? 'light' : 'dark') + " mode");
     });
 
     ribbonIconEl.addClass('ribbon-theme-toggle-plugin');
